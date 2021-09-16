3fe502e619e4b918726778436386db09d00b0fed
Arsalaan Ali <arsalaanali2@gmail.com>
2021-09-15 23:34:01 -0400

    Info Class

---
+++ inventory/src/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>Inventory System</h1>
+            <p>Manage Your Stuff</p>
+            </div>
+        );
+    }
+}
+export default Info;
